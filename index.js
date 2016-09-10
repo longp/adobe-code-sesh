8074c28c680a13c3de7e63ea241a4388d52a0be7
long phan <lphan1989@gmail.com>
2016-09-10 17:15:14 -0400

    img is succesfully editable

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
-  $("#edit").click(function() {
-    console.log("ayy lmao")
+  var featherEditor = new Aviary.Feather({
+        apiKey: '83a45835af9f4000939ce3abddcf48d8',
+        onSave: function(imageID, newURL) {
+            var img = $('#img')[0];
+            img.src = newURL;
+            featherEditor.close();
 
-  })
+        }
+    });
+
+    $("#edit").click(function() {
+      var targetImage = $('#img')[0];
+      launchEditor(targetImage.id, targetImage.src)
+    })
+
+    function launchEditor(id, src) {
+        featherEditor.launch({
+            image: id,
+            url: src
+        });
+        return false;
+    }
+
 })
